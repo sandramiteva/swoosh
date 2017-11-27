f672f6f0ffd8040876de5ac87957a2884873e14a
Sandra <sandra@Sandras-Mac.local>
2017-11-27 17:48:07 +0100

    Lesson 9 done enable/disable next button

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +23,22 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillSegueVC", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Sandra on 11/27/17.
+//  Copyright © 2017 Sandra. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague : String?
+    var selectedSkillLevel : String?
+}
